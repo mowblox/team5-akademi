cad4342944218ca9af7eb59585406282df822f27
iamsirmike <asamoahmichael77@gmail.com>
2023-08-10 15:04:10 +0000

    write donation contract

---
+++ blockhain/scripts/deploy_donate.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const donate = await ethers.deployContract("Donate");
+
+  console.log("Loan address:", await donate.getAddress());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
